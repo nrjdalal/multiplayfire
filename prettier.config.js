c1a5c72d63f1ddc5444ef8c361dd920f656a8be4
Neeraj Dalal <admin@nrjdalal.com>
2024-09-03 00:42:46 +0530

    added mobile only banner

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   semi: false,
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
 }
